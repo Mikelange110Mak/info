0a4629566bd1545ac1e8cb1316bcb4111b28e26b
Mak <infinitycorpkz@gmail.com>
2022-11-11 11:35:30 +0600

    fetchApi

--- js/script.js
+++ js/script.js
@@ -13,9 +13,17 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    }
 
+   const resultOutput = document.querySelector('.period__result'),
+      monthSelector = document.querySelector('.period__month-input');
 
+   let selectedValue = monthSelector.options[month.selectedIndex].value;
+   console.log(selectedValue);
+
+   let dayValue = document.querySelector('.period__day-input').value
+   console.log(dayValue);
 
    function req() {
+      /*
       const request = new XMLHttpRequest();
 
       //Получение GET запроса, по адресу: (json-server)
@@ -39,9 +47,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
             //Скопипастил свои функции:
+            
             function totalTime(arr) {
                const totalCount = arr.reduce((acc, curr) => acc + curr.time, 0)
                console.log(Math.ceil(totalCount / 60));
+               resultOutput.innerHTML = `
+               <div class="period__result-hours">${Math.ceil(totalCount / 60)} часов</div>
+               `
             }
             totalTime(data)
 
@@ -50,22 +62,74 @@ window.addEventListener('DOMContentLoaded', () => {
                const countTime = filterMonth.reduce((total, curr) => {   //сумма времени
                   return total + curr.time
                }, 0)
+
+               let monthText = '';
+               switch (true) {
+                  case (month === 1): monthText = 'Январь'; break;
+                  case (month === 2): monthText = 'Февраль'; break;
+                  case (month === 3): monthText = 'Март'; break;
+                  case (month === 4): monthText = 'Апрель'; break;
+                  case (month === 5): monthText = 'Май'; break;
+                  case (month === 6): monthText = 'Июнь'; break;
+                  case (month === 7): monthText = 'Июль'; break;
+                  case (month === 8): monthText = 'Август'; break;
+                  case (month === 9): monthText = 'Сентябрь'; break;
+                  case (month === 10): monthText = 'Октябрь'; break;
+                  case (month === 11): monthText = 'Ноябрь'; break;
+                  case (month === 12): monthText = 'Декабрь'; break;
+               }
+
+               if (countTime !== 0) {
+                  resultOutput.innerHTML = `
+      <div class="period__result-hours">${Math.ceil(countTime / 60)} часов за ${monthText} месяц</div>
+      `;
+               } else {
+                  resultOutput.innerHTML = `
+      <div class="period__result-hours">Нет данных за ${monthText} месяц</div>
+      `;
+               }
+
                console.log(Math.ceil(countTime / 60));
             }
-            timeByMonth(data, 8)
-            ////
-
+            timeByMonth(data, 11)
 
 
          } else console.error('Что-то пошло не так :(')
 
-
-
       })
+      */
+
+      /*
+      //Создаем запрос
+      fetch("http://localhost:3000/timeForCode")
+         //Получаем данные, и у фетча есть метод парса json
+         .then(data => data.json())
+         //После этого по цепочке, его надо обработать, я вызываю функцию
+         .then(data => timeByMonth(data, 11))
+         //обработать ошибки:
+         .catch(err => console.error(err))
+         */
+
+      //Вместо fetch запроса выше, использую обновленный со вспомогательной функцией:
+
+      getResource("http://localhost:3000/timeForCode")
+         .then(data => timeByMonth(data.data, 8))
+         .catch(err => console.error(err))
+
    }
 
    req()
 
+
+   //Вспомогательная функция которая будет оптимизировать и проверять fetch запросы
+   async function getResource(url) {
+      const res = await axios(`${url}`);
+
+      //внутри фетча, появляется свойство ок, которое понятно из названия говорит о том что запрос выполнен
+      if (res.status !== 200) throw new Error(`Couldn't fetch ${url}, status ${res.status}`);
+
+      return await res;
+   }
 
 
    /*
@@ -75,19 +139,47 @@ window.addEventListener('DOMContentLoaded', () => {
          console.log(Math.ceil(totalCount / 60));
       }
       totalTime(data)
-   
-   
-     
-      //Количество затраченных часов по месяцам
-      function timeByMonth(arr, month) {
-         const filterMonth = arr.filter(item => item.month === month) //отфильтрованные месяца по аргументу функции
-         const countTime = filterMonth.reduce((total, curr) => {   //сумма времени
-            return total + curr.time
-         }, 0)
-         console.log(Math.ceil(countTime / 60));
+   */
+
+
+
+   //Количество затраченных часов по месяцам
+   function timeByMonth(arr, month) {
+      const filterMonth = arr.filter(item => item.month === month) //отфильтрованные месяца по аргументу функции
+      const countTime = filterMonth.reduce((total, curr) => {   //сумма времени
+         return total + curr.time
+      }, 0)
+
+      let monthText = '';
+      switch (true) {
+         case (month === 1): monthText = 'Январь'; break;
+         case (month === 2): monthText = 'Февраль'; break;
+         case (month === 3): monthText = 'Март'; break;
+         case (month === 4): monthText = 'Апрель'; break;
+         case (month === 5): monthText = 'Май'; break;
+         case (month === 6): monthText = 'Июнь'; break;
+         case (month === 7): monthText = 'Июль'; break;
+         case (month === 8): monthText = 'Август'; break;
+         case (month === 9): monthText = 'Сентябрь'; break;
+         case (month === 10): monthText = 'Октябрь'; break;
+         case (month === 11): monthText = 'Ноябрь'; break;
+         case (month === 12): monthText = 'Декабрь'; break;
       }
-      timeByMonth(timeForCode, 10)
-      */
+
+      if (countTime !== 0) resultOutput.innerHTML = `
+      <div class="period__result-hours">${Math.ceil(countTime / 60)} часов за ${monthText} месяц</div>
+      `;
+
+      else resultOutput.innerHTML = `
+       <div class="period__result-hours">Нет данных за ${monthText} месяц</div>
+       `;
+
+      console.log(Math.ceil(countTime / 60));
+
+
+   }
+
+
 
 })
 
